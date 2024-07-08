a38545a5f83c3f11ec67d517181682f24b9acaf5
linux_at_TUM <balabala@in.tum.de>
2024-07-08 19:46:08 +0200

    1

--- snake_ar_project/scripts/camera.gd
+++ snake_ar_project/scripts/camera.gd
@@ -16,10 +16,10 @@ func _ready():
 func _process(delta):
 	if game.game_over or global.interface_to_display != global.DISPLAY_GAMEPLAY:
 		return
-	var refresh_time = game.default_refresh_time * current_refresh_multiplier
-	game.update_timer.set_wait_time(refresh_time)
-	new_transform.origin -= backward_vector * delta / refresh_time
-	smoothly_move(new_transform, refresh_time * smoothness_multiplier)
+#	var refresh_time = game.default_refresh_time * current_refresh_multiplier
+#	game.update_timer.set_wait_time(refresh_time)
+#	new_transform.origin -= backward_vector * delta / refresh_time
+#	smoothly_move(new_transform, refresh_time * smoothness_multiplier)
 
 func smoothly_move(transform_to, duration):
 	game.tween.interpolate_property(self, "transform", transform, transform_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN)
